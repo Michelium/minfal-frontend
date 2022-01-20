c3fb6218b5f2ba8791ea75f0797da0afe3d43401
Michel Hamelink <info@michelhamelink.nl>
2022-01-20 13:22:15 +0100

    updated location custom handle icon are now from categories

--- src/home/components/LocationDetailsCard/LocationDetailsCustomHandle.js
+++ src/home/components/LocationDetailsCard/LocationDetailsCustomHandle.js
@@ -3,21 +3,26 @@ import {StyleSheet} from 'react-native';
 
 import {Button, Text, View} from '../../../common';
 import CinemaIcon from '../../../assets/icons/edit-cinema-icon-large.svg';
+import MeatIcon from '../../../assets/icons/filter-meat-icon.svg';
+import ChessIcon from '../../../assets/icons/filter-chess-icon.svg';
+import StrawberryIcon from '../../../assets/icons/filter-strawberry-icon.svg';
+import FishIcon from '../../../assets/icons/filter-fish-icon.svg';
+import TeaIcon from '../../../assets/icons/filter-tea-icon.svg';
 import * as Colors from '../../../config/colors';
 
 // locationDetailsCustomHandle is a function that takes arguments, and returns in turn a new function that uses these arguments.
 // Thus, we return a function that we can use as handler in the component, by calling locationDetailsCustomHandle(arguments) we get this function.
 // In the component, this will look like: onChangeHandler={locationDetailsCustomHandle(arguments)}.
 const locationDetailsCustomHandle = (location) => {
-  console.log("This gets called as soon as the component is loaded, in order to set the handler");
+  // console.log("This gets called as soon as the component is loaded, in order to set the handler");
   return () => {
-    console.log("this gets callend when the handler get's called");
-    console.log(location);
+    // console.log("this gets callend when the handler get's called");
+    // console.log(location);
     return (
     <View horizontal style={styles.introContainer}>
     <View style={styles.handler} />
     <View horizontal>
-      <CinemaIcon />
+      {logo(location.category)}
       <Text lg style={styles.introTxt}>
         {location.category}
       </Text>
@@ -59,6 +64,25 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
+
+const logo = function(category) {
+  switch (category) {
+    case 'Bioscoop':
+      return <CinemaIcon/>
+    case 'Slager':
+      return <MeatIcon/>
+    case 'Kaasboer':
+      return <ChessIcon/>
+    case 'Groenteboer':
+      return <StrawberryIcon/>
+    case 'Visboer':
+      return <FishIcon/>
+    case 'Koffietent':
+      return <TeaIcon/>
+    default:
+      return <CinemaIcon/>
+  }
+};
 
 /* Export
 ============================================================================= */
